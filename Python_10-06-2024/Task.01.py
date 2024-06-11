6db39a412741d523ff5f90f2c74a8869fa85829e
Ashish Kumar <ashishtripathi4186@gmail.com>
2024-06-12 00:20:23 +0530

    Task 1 - Done

--- Python_10-06-2024/Task.01.py
+++ Python_10-06-2024/Task.01.py
@@ -1,1 +1,41 @@
-print("Hello World")
+# 1. Explain the difference between the = operator and the == operator in Python.
+# The = operator is used to assign a value.
+# The == operator is used to compare two values and return True if they are equal.
+
+# Example:
+x = 10
+print(x)
+
+x = 50
+y = 60
+print(x + y)
+print(x == y)
+
+a = "Ashish is a QA Analyst"
+print(a)
+print(a == "Ashish is a QA Analyst")
+
+# 2. What does the ** operator do in Python, and how is it used?
+# The ** operator is used for exponential / Power.
+# Example:
+print(3 ** 3)
+
+x = 7
+y = 2
+print(x ** y)  # Output: 49 (7 * 7)
+
+# What does the ^ operator do in Python, and in what context is it commonly used?
+# The ^ operator is the bitwise XOR operator. It performs a bitwise XOR operation on two integers.
+# Example:
+print(3 ^ 3)
+print(3 ^ 2)
+print(3 ^ 4)
+print(4 ^ 3)
+print(4 ^ 2)
+print(4 ^ 4)
+
+x = 6
+y = 7
+print(x ^ y)  # Output: 3 (6 ^ 7)
+
+
