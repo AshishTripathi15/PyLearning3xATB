5f7135134648216f082428d2d520a85bf7b59de0
Ashish Kumar <ashishtripathi4186@gmail.com>
2024-06-12 00:40:59 +0530

    Task 2 - Done

---
+++ Python_10-06-2024/Task#02.py
@@ -0,0 +1,30 @@
+# 1. Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+
+num = 2
+sq = num ** 2
+print(sq)
+
+c = num ** 3
+print(c)
+
+num = 2
+print(f"Square of {num} is {sq}", f"Cube of {num} is {c}")
+
+num = 2
+print((num ** 2),"\n",(num ** 3))
+
+
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+x = 49
+y = 78
+print (x > y)
+print (x < y)
+print (x == y)
+
+
+
+
+
+
+
